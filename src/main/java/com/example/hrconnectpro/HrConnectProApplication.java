2bd02df79040ac7c046b07744e9a8458d4763953
hajjou walkadi <walkadihajjou153@gmail.com>
2024-01-09 11:11:28 +0100

    First Commit

---
+++ src/main/java/com/example/hrconnectpro/HrConnectProApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrconnectpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConnectProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConnectProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hrconnectpro/HrConnectProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrconnectpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConnectProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
