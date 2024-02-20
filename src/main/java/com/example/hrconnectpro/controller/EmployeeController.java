8d65e5f18ae003c2453bd07d0c990fb02373c7bd
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-20 16:00:30 +0100

    CRUD of Employee was added successfully

--- src/main/java/com/example/hrconnectpro/controller/EmployeeController.java
+++ src/main/java/com/example/hrconnectpro/controller/EmployeeController.java
@@ -1,2 +1,66 @@
-package com.example.hrconnectpro.controller;public class EmployeeController {
+package com.example.hrconnectpro.controller;
+
+import com.example.hrconnectpro.config.handlers.response.ResponseMessage;
+import com.example.hrconnectpro.entities.Employee;
+import com.example.hrconnectpro.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployee();
+        if(employees.isEmpty()) {
+            return ResponseMessage.notFound("Employee not found");
+        }else {
+            return ResponseMessage.ok(employees,"Success");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getEmployeeById(Long id) {
+        return ResponseMessage.ok(employeeService.getEmployeeById(id), "Success");
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity addEmployee(@RequestBody Employee employee) {
+        Employee employee1 = employeeService.addEmployee(employee);
+        if(employee1 == null) {
+            return ResponseMessage.badRequest("Employee not created");
+        }else {
+            return ResponseMessage.created(employee1,"Employee created successfully");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+        Employee employee1 = employeeService.updateEmployee(employee, id);
+        if(employee1 == null) {
+            return ResponseMessage.badRequest("Employee not updated");
+        }else {
+            return ResponseMessage.created(employee1, "Employee updated successfully");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if(employee == null) {
+            return ResponseMessage.notFound("Employee not found");
+        }else {
+            employeeService.deleteEmployee(id);
+            return ResponseMessage.ok("Employee deleted successfully", null);
+        }
+    }
+
 }
