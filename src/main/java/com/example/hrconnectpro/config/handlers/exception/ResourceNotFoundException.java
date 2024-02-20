8035573efa7a875153baa8c5d3e1235a0c9ebf4b
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-20 15:21:55 +0100

    ResourceNotFoundException was added successfully

--- src/main/java/com/example/hrconnectpro/config/handlers/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/hrconnectpro/config/handlers/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.aftas.config.handlers.exception;
+package com.example.hrconnectpro.config.handlers.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
