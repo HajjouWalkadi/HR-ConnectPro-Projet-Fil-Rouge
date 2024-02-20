14e767ee429d5b6156bbb1edb5609bd4b8d4446e
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-20 15:56:07 +0100

    EmployeeRepository was added successfully

--- src/main/java/com/example/hrconnectpro/repository/EmployeeRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/EmployeeRepository.java
@@ -1,2 +1,13 @@
-package com.example.hrconnectpro.repository;public interface EmployeeRepository {
+package com.example.hrconnectpro.repository;
+
+import com.example.hrconnectpro.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Employee> findByEmail(String email);
 }
