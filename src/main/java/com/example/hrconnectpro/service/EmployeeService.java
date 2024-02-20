f7b9adb22bc116eabbb813ebf85aa22f62c80829
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-20 15:59:22 +0100

    EmployeeService and employeeServiceImpl were added successfully

--- src/main/java/com/example/hrconnectpro/service/EmployeeService.java
+++ src/main/java/com/example/hrconnectpro/service/EmployeeService.java
@@ -1,2 +1,17 @@
-package com.example.hrconnectpro.service;public interface EmployeeService {
+package com.example.hrconnectpro.service;
+
+import com.example.hrconnectpro.entities.Employee;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee getEmployeeById(Long id);
+    Employee addEmployee(Employee employee);
+    List<Employee> getAllEmployee();
+    Employee updateEmployee(Employee employee, Long id);
+    void deleteEmployee(Long id);
+
+    //List<Employee> getAllEmployeesPaginated(Pageable pageable);
 }
+
--- src/main/java/com/example/hrconnectpro/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/hrconnectpro/service/impl/EmployeeServiceImpl.java
@@ -1,2 +1,49 @@
-package com.example.hrconnectpro.service.impl;public class EmployeeServiceImpl {
+package com.example.hrconnectpro.service.impl;
+
+import com.example.hrconnectpro.entities.Employee;
+import com.example.hrconnectpro.repository.EmployeeRepository;
+import com.example.hrconnectpro.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, Long id) {
+        employee.setFirstName(employee.getFirstName());
+        employee.setLastName(employee.getLastName());
+        employee.setEmail(employee.getEmail());
+        employee.setTelephone(employee.getTelephone());
+        employee.setPoste(employee.getPoste());
+        employee.setSalaire(employee.getSalaire());
+
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
