63eff9f02b50a4f8f80b476f7f99eb755abb7c74
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-22 18:44:59 +0100

    CongeDTO was added successfully

--- src/main/java/com/example/hrconnectpro/dto/CongeDTO.java
+++ src/main/java/com/example/hrconnectpro/dto/CongeDTO.java
@@ -1,2 +1,41 @@
-package com.example.hrconnectpro.dto;public record CongeDTO() {
+package com.example.hrconnectpro.dto;
+
+import com.example.hrconnectpro.entities.Conge;
+import com.example.hrconnectpro.entities.Employee;
+import com.example.hrconnectpro.entities.Role;
+import com.example.hrconnectpro.enums.TypeConge;
+
+import java.time.LocalDate;
+
+public record CongeDTO(
+        Long id,
+        LocalDate dateDebut,
+        LocalDate dateFin,
+        String employeeId,
+        String employeeName,
+        String employeeEmail,
+        String employeeRole,
+        TypeConge typeConge) {
+
+    public Conge toConge() {
+        return Conge.builder()
+                .id(id)
+                .typeConge(typeConge)
+                .dateDebut(dateDebut)
+                .dateFin(dateFin)
+                .employee(
+                        Employee.builder()
+                                // Check if employeeId is not null before parsing
+                                .id(employeeId != null ? Long.parseLong(employeeId) : null)
+                                .lastName(employeeName)
+                                .email(employeeEmail)
+                                .role(
+                                        Role.builder()
+                                                .name(employeeRole)
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
+    }
 }
