6032457d1cb0728c33dec0d7b7b3ab5adee05664
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-22 18:41:22 +0100

    CongeRepository was added successfully

--- src/main/java/com/example/hrconnectpro/repository/CongeRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/CongeRepository.java
@@ -1,2 +1,9 @@
-package com.example.hrconnectpro.repository;public interface CongeRepository {
+package com.example.hrconnectpro.repository;
+
+import com.example.hrconnectpro.entities.Conge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CongeRepository extends JpaRepository<Conge, Long> {
+    Conge findCongeByEmployeeId(Long id);
+    Conge findByName(String name);
 }
