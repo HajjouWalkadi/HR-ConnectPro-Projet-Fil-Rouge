fbb46ca92e9f0523845f6aa2d5f269e4ba31b73e
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-22 21:01:45 +0100

    DepartementRepository was modified successfully

--- src/main/java/com/example/hrconnectpro/repository/DepartementRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/DepartementRepository.java
@@ -1,2 +1,9 @@
-package com.example.hrconnectpro.repository;public interface DepartementRepository {
+package com.example.hrconnectpro.repository;
+
+import com.example.hrconnectpro.entities.Departement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartementRepository extends JpaRepository<Departement, Long> {
+    Departement findByName(String name);
+
 }
