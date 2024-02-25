27c2e5f78487ad6b66caddd59ecc227ff63a089d
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-25 14:23:19 +0100

    PosteRepository was modified successfully

--- src/main/java/com/example/hrconnectpro/repository/PosteRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/PosteRepository.java
@@ -1,2 +1,10 @@
-package com.example.hrconnectpro.repository;public interface PosteRepository {
+package com.example.hrconnectpro.repository;
+
+import com.example.hrconnectpro.entities.Poste;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PosteRepository extends JpaRepository<Poste, Long> {
+    //Poste findPosteByEmployeeId(Long id);
+    Poste findByName(String name);
+
 }
