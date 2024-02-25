652258b2ce4431d7f47d2f379e9a74a342f68a78
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-25 14:24:00 +0100

    Methods in PosteService were added successfully

--- src/main/java/com/example/hrconnectpro/service/PosteService.java
+++ src/main/java/com/example/hrconnectpro/service/PosteService.java
@@ -1,2 +1,12 @@
-package com.example.hrconnectpro.service;public interface PosteService {
+package com.example.hrconnectpro.service;
+
+import com.example.hrconnectpro.entities.Employee;
+import com.example.hrconnectpro.entities.Poste;
+
+import java.util.List;
+
+public interface PosteService {
+    Poste getPosteById(Long id);
+    Poste addPoste(Poste poste);
+    List<Poste> getAllPostes();
 }
