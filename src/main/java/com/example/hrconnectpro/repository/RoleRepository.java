94dbc02df9791d929b06a255b46230d63258f0b0
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-25 14:20:09 +0100

    RoleRepository was modified successfully

--- src/main/java/com/example/hrconnectpro/repository/RoleRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/RoleRepository.java
@@ -1,6 +1,6 @@
-package com.example.aftas.repository;
+package com.example.hrconnectpro.repository;
 
-import com.example.aftas.model.Role;
+import com.example.hrconnectpro.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
