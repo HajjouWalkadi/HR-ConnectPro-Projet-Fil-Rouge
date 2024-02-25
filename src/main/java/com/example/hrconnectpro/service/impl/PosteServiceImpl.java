22d5b9c8d7a9cdd430335f8efca7508aca849dac
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-25 14:24:30 +0100

    Methods in PosteServiceImpl were implemented successfully

--- src/main/java/com/example/hrconnectpro/service/impl/PosteServiceImpl.java
+++ src/main/java/com/example/hrconnectpro/service/impl/PosteServiceImpl.java
@@ -1,2 +1,34 @@
-package com.example.hrconnectpro.service.impl;public class PosteServiceImpl {
+package com.example.hrconnectpro.service.impl;
+
+import com.example.hrconnectpro.config.handlers.exception.ResourceNotFoundException;
+import com.example.hrconnectpro.entities.Employee;
+import com.example.hrconnectpro.entities.Poste;
+import com.example.hrconnectpro.repository.PosteRepository;
+import com.example.hrconnectpro.service.PosteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PosteServiceImpl implements PosteService {
+    private final PosteRepository posteRepository;
+    @Override
+    public Poste getPosteById(Long id) {
+      return posteRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Poste addPoste(Poste poste) {
+        if (posteRepository.findByName(poste.getNom()) != null) {
+            throw new ResourceNotFoundException("Conge name " + poste.getNom() + " already exists");
+        }
+        return posteRepository.save(poste);
+    }
+
+    @Override
+    public List<Poste> getAllPostes() {
+        return posteRepository.findAll();
+    }
 }
