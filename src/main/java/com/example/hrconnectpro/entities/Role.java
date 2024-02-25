003e3af7dd0480044b678ec5757c91fc08d0b88f
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-25 14:19:38 +0100

    Role class was modified successfully

--- src/main/java/com/example/hrconnectpro/entities/Role.java
+++ src/main/java/com/example/hrconnectpro/entities/Role.java
@@ -1,2 +1,26 @@
-package com.example.hrconnectpro.entities;public class Role {
+package com.example.hrconnectpro.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    //@ManyToMany(fetch = FetchType.EAGER )
+    //private List<Authority> authorities;
+    private boolean isDefault;
+
+
 }
