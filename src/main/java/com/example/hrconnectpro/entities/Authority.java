a041190a4a6bc9959960e5f9124f011ff520d4aa
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-26 19:37:06 +0100

    Authority class was modified

--- src/main/java/com/example/hrconnectpro/entities/Authority.java
+++ src/main/java/com/example/hrconnectpro/entities/Authority.java
@@ -1,2 +1,26 @@
-package com.example.hrconnectpro.entities;public class Authority {
+package com.example.hrconnectpro.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(value = {"roles"}, allowSetters = true)
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToMany(mappedBy = "authorities")
+//    private List<Role> roles;
+
+
+    private String name;
 }
