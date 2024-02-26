174195b4a97e3111257ce83278f455bf0ce104a8
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-26 20:29:43 +0100

    Methods in FormationService were added successfully

--- src/main/java/com/example/hrconnectpro/service/FormationService.java
+++ src/main/java/com/example/hrconnectpro/service/FormationService.java
@@ -1,2 +1,12 @@
-package com.example.hrconnectpro.service;public interface FormationService {
+package com.example.hrconnectpro.service;
+
+import com.example.hrconnectpro.entities.Formation;
+
+import java.util.List;
+
+public interface FormationService {
+    Formation saveFormation(Formation formation);
+    Formation findFormationById(Long id);
+    void deleteFormation(Long id);
+    List<Formation> getAllFormations();
 }
