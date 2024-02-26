c6197e40fec8b0ac460a38d22f5251a85f717a04
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-26 20:28:31 +0100

    FormationRepository was modified successfully

--- src/main/java/com/example/hrconnectpro/repository/FormationRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/FormationRepository.java
@@ -1,2 +1,8 @@
-package com.example.hrconnectpro.repository;public interface FormationRepository {
+package com.example.hrconnectpro.repository;
+
+import com.example.hrconnectpro.entities.Formation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormationRepository extends JpaRepository<Formation, Long> {
+    Formation findByTitre(String name);
 }
