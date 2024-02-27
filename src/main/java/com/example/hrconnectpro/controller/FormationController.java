87638883c8db8b72d6ff26594eff98463bbd099f
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-27 15:43:04 +0100

    Formation CRUD methods were added successfully

--- src/main/java/com/example/hrconnectpro/controller/FormationController.java
+++ src/main/java/com/example/hrconnectpro/controller/FormationController.java
@@ -1,2 +1,48 @@
-package com.example.hrconnectpro.controller;public class FormationController {
+package com.example.hrconnectpro.controller;
+
+import com.example.hrconnectpro.config.handlers.response.ResponseMessage;
+import com.example.hrconnectpro.entities.Formation;
+import com.example.hrconnectpro.service.FormationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/formations")
+@RequiredArgsConstructor
+public class FormationController {
+    private final FormationService formationService;
+
+    @GetMapping
+    public ResponseEntity getAllFormations() {
+        List<Formation> formations = formationService.getAllFormations();
+        if(formations.isEmpty()) {
+            return ResponseMessage.notFound("Formation not found");
+        }else {
+            return ResponseMessage.ok(formations, "Success");
+        }
+    }
+    @PostMapping("/add")
+    public ResponseEntity saveFormation(Formation formation) {
+        Formation formation1 = formationService.saveFormation(formation);
+        if(formation1 == null) {
+            return ResponseMessage.badRequest("Formation not created");
+        }else {
+            return ResponseMessage.created(formation1, "Formation created successfully");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getFormationById(Long id) {
+        return ResponseMessage.ok( formationService.findFormationById(id), "Success");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteFormation(Long id) {
+        formationService.deleteFormation(id);
+        return ResponseMessage.ok(formationService.findFormationById(id), "Formation deleted successfully");
+    }
+
 }
