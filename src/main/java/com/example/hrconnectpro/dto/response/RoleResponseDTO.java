20a69ce573d2845bce089a548f381a5a1aad746b
hajjou walkadi <walkadihajjou153@gmail.com>
2024-02-28 16:30:55 +0100

    RoleResponseDTO was modified

--- src/main/java/com/example/hrconnectpro/dto/response/RoleResponseDTO.java
+++ src/main/java/com/example/hrconnectpro/dto/response/RoleResponseDTO.java
@@ -1,21 +1,18 @@
-package com.example.aftas.dto.response;
+package com.example.hrconnectpro.dto.response;
 
-import com.example.aftas.enums.AuthorityEnum;
-import com.example.aftas.model.Authority;
-import com.example.aftas.model.Role;
 
-import java.util.List;
+import com.example.hrconnectpro.entities.Role;
+
 
 
 public record RoleResponseDTO(
         String name,
-        List<String> authorities,
+
         boolean isDefault
 ) {
     public static RoleResponseDTO fromRole(Role role){
         return new RoleResponseDTO(
                 role.getName(),
-                role.getAuthorities().stream().map(Authority::getName).toList(),
                 role.isDefault()
         );
     }
