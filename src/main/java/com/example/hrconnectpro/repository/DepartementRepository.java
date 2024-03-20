3b539744a00d7269eb541400c1f2184f64d534ab
hajjou walkadi <walkadihajjou153@gmail.com>
2024-03-20 13:47:47 +0000

    These files likely contain repositories representing like DepartmentRepository and PosteRepository

--- src/main/java/com/example/hrconnectpro/repository/DepartementRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/DepartementRepository.java
@@ -3,7 +3,9 @@ package com.example.hrconnectpro.repository;
 import com.example.hrconnectpro.entities.Departement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DepartementRepository extends JpaRepository<Departement, Long> {
-    Departement findByNom(String name);
+    Optional<Departement> findByNom(String name);
 
 }
--- src/main/java/com/example/hrconnectpro/repository/PosteRepository.java
+++ src/main/java/com/example/hrconnectpro/repository/PosteRepository.java
@@ -3,8 +3,11 @@ package com.example.hrconnectpro.repository;
 import com.example.hrconnectpro.entities.Poste;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PosteRepository extends JpaRepository<Poste, Long> {
     //Poste findPosteByEmployeeId(Long id);
-    Poste findByNom(String name);
+    //Poste findByNom(String name);
 
+    Optional<Poste> findByNom(String nom);
 }
